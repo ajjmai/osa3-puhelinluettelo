bb30300fcb6bb4e02c0a17b49827a782c674837f
Aija Valleala <aija.valleala@aalto.fi>
2019-04-19 10:07:01 +0300

    tehtävä 3.8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+morgan.token("data", req => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
